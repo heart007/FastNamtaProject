0a290d948bed835583ae1b3a8ec24309bcbdd16f
unknown <yeachir15-7459@diu.edu.bd>
2019-09-10 00:54:36 +0600

    Multiplication with exit code

--- namta.py
+++ namta.py
@@ -1,30 +1,13 @@
 def calc ():
+	number = int(input("Enter the Number : "))
 	for i in range(1,11):
 		print (str(number) + " * " + str(i)+ " = " +str(round(number*i)))
 		i=i+1
-try:
-	number = int(input("Enter the Number : "))
-	print("it's a int")
-	# calc()
-	# continue
-except:
-	print("it's a string")
-	# entry = input("Press Y for continue, or Press N to Exit: ").lower()
-	# if (entry == 'y'):
-	# 	number = int(input("Enter the Number : "))
-	# 	calc() 
-	# else:
-	# 	exit()
-
-
-
-	# while True:	
-	# entry = input("Press Y for continue, or Press N to Exit: ").lower()
-	# if (entry == 'y'):
-	# 	calc() 
-	# else:
-	# 	exit()
-
-
-# calc ()		
-
+calc()
+while True:		
+	entry = input("Press Y for continue, or Press N to Exit: ").lower()
+	if (entry == 'y'):
+		calc()
+		continue
+	else:
+		exit()
